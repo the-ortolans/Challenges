4e73131f1479de79256efcd90e3c3c5560285d9b
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-24 20:00:12 -0500

    added konami code audio file

--- js/thankunext-refactored.js
+++ js/thankunext-refactored.js
@@ -34,6 +34,11 @@ window.onload = function () {
     var audioVox = document.createElement('audio');
     audioVox.setAttribute('src', 'snd/thankunext-next.mp3');
 
+    var audioKon = document.createElement('audio');
+    audioKon.setAttribute('src', 'snd/thankunext-next-expl.mp3');
+
+
+
     document.getElementById("yes").onclick = function () {
         position++;
         document.getElementById('questions')
@@ -60,6 +65,52 @@ window.onload = function () {
 
     document.getElementById('questions')
         .innerHTML = questions[position];
+
+
+
+    var konamiArr = [
+        "ArrowUp",
+        "ArrowUp",
+        "ArrowDown",
+        "ArrowDown",
+        "ArrowLeft",
+        "ArrowRight",
+        "ArrowLeft",
+        "ArrowRight",
+        "b",
+        "a"
+    ];
+
+    var pushArr = [];
+
+    $(document).keyup(function(event){
+
+        var konamid = true;
+
+        pushArr.push(event.key);
+
+        if (pushArr.length > 10){
+            pushArr.shift();
+        }
+
+        for (var i = 0; i < konamiArr.length; i++) {
+
+            if (konamiArr[i] !== pushArr[i]){
+                return konamid = false;}
+        }
+
+        if (konamid && pushArr.length === 10) {
+
+            if (audioInst.play() || audioVox.play()) {
+                audioInst.pause();
+            }
+
+            audioKon.play();
+
+
+        }
+    });
+
 
 };
 
