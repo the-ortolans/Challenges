5991ffc61171b60f5cfdf3246be1681b9c7a7cc2
Nadia Botello <botello.nadia.g@gmail.com>
2019-04-25 15:00:11 -0500

    first part of browser drawing synth

---
+++ js/browsersynth.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function setup() {
+    createCanvas(1200, 1200);
+}
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+}
+
