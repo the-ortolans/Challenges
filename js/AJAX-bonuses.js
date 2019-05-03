1f8c19325b0fa1efed8912c7d1ccb14a32c45abe
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-02 19:21:43 -0500

    update

---
+++ js/AJAX-bonuses.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+    function jokesHtml(jokes) {
+
+        var html = '';
+
+        jokes.forEach(function(joke) {
+            html += '<div class="questions mx-auto" id="questions">';
+            html += '<h1>' + joke.quote + '</h1>';
+            html += '</div>';
+        });
+
+        return html;
+    }
+
+
+            $.ajax('http://quotes.stormconsultancy.co.uk/random.json').done(function (jokes) {
+                console.log(jokes.quote);
+                // $('#questions').html(jokesHtml(jokes));
+            }).fail(function () {
+                console.log("ERROR!");
+            });
+
+
+
--- js/browsersynth.js
+++ js/browsersynth.js
@@ -1,15 +1,38 @@
-"use strict";
+
+// function setup() {
+//     createCanvas(1200, 1200);
+//     song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
+// }
+//
+// function draw() {
+//     if (mouseIsPressed) {
+//         fill(0);
+//     } else {
+//         fill(255);
+//     }
+//
+//     ellipse(mouseX, mouseY, 80, 80);
+//
+// }
+
+
+
+
+
+
+var song = loadSound('/snd/thankunext-next.mp3');
 
 function setup() {
     createCanvas(1200, 1200);
 }
 
-function draw() {
-    if (mouseIsPressed) {
-        fill(0);
+function mousePressed() {
+    if (song.isPlaying()) {
+        // .isPlaying() returns a boolean
+        song.stop();
+        background(255, 0, 0);
     } else {
-        fill(255);
+        song.play();
+        background(0, 255, 0);
     }
-    ellipse(mouseX, mouseY, 80, 80);
 }
-
